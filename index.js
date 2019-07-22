e7b7cd0e31b51f37fb8e47927ddea7a013a3b1c1
Nimantha Nadeeraka <info.nadeeraka@gmail.com>
2019-07-22 16:30:46 +0530

    all functions work with minor bugs

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 "use strict";
-// check wather jquery import properly or not
-
-if (!jQuery) {
-  alert("jQuery cdn  problem");
-}
 
 const red_border = {
-  border: "2px solid red"
+  border: "3px solid #e73c3d "
 };
 
 const green_border = {
-  border: "2px solid green"
+  border: "3px solid #29eb07"
 };
-//Request URL: https://pozzad-email-validator.p.rapidapi.com/emailvalidator/validateEmail/{email}
 
-//event listener
-$("._select").on("change", function() {
+$("#_select").on("change", function() {
   apiCall();
 });
 
 const apiCall = () => {
-  const email = $("._select")
+  const email = $("#_select")
     .val()
     .trim();
-  //console.log(value);
 
   const request = $.ajax({
     url: `https://pozzad-email-validator.p.rapidapi.com/emailvalidator/validateEmail/${email}`,
@@ -35,11 +27,10 @@ const apiCall = () => {
       "X-RapidAPI-Key": "a440e3a4c3msh204eb7641a0b400p1ae281jsn4560ea04dc58"
     }
   }).done(function(data) {
-    console.log(data);
     if (data.isValid === false) {
-      return $("._select").css(red_border);
+      return $("#_select").css(red_border);
     } else {
-      return $("._select").css(green_border);
+      return $("#_select").css(green_border);
     }
   });
 };
